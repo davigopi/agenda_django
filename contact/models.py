0f0e9441cd06d859d912ad27859da1d14814cafa
davigopi <davigopi@gmail.com>
2024-06-22 13:33:31 -0300

    2206

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
+# owner (foreign key)
 
-# Create your models here.
+class Category(models.Model):
+    # https://docs.djangoproject.com/en/5.0/ref/models/options/
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)   # blank=True define como opcional
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,  # apagar categoria contatos null
+        blank=True, null=True
+    ) # on_delete=models.CASCADE,  -> apagar categoria apaga contatos com categoria
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+ 
--- project/settings.py
+++ project/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -117,8 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
 STATICFILES_DIRS = (BASE_DIR / 'base_static', )
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
